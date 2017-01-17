08602fb7a875eeee6eb18c00ca560caafc589a7b
mordant23 <mordant23@gmail.com>
2017-01-17 00:01:03 -0800

    squashing bugs

--- rootfs/opt/mariadb/init.sql
+++ rootfs/opt/mariadb/init.sql
@@ -3,3 +3,7 @@ CREATE USER 'root'@'%' IDENTIFIED BY '<<root_password>>';
 GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
 DROP DATABASE test;
 
+CREATE DATABASE <<database>>;
+CREATE USER '<<database_user>>'@'%' IDENTIFIED BY '<<database_user_password>>';
+GRANT ALL ON `<<database>>`.* TO '<<database_user>>'@'%';
+FLUSH PRIVILEGES;
